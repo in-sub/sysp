e57693676803e98b2b664d217b80739b0ea6ea35
sw-subeen <sw.subeen@gmail.com>
2016-12-16 00:57:52 +0900

    final
    Signed-off-by: sw-subeen <sw.subeen@gmail.com>

---
+++ final/crossyRoad.h
@@ -0,0 +1,32 @@
+#define BLANK           ' '
+#define BALL_SYMBOL     '8'
+
+#define ROAD_SYMBOL 	"----------------------------------------------------------------"
+#define BNDR_SYMBOL     '+'                             //경계선 심볼
+#define OBS_SYMBOL	"OOOOOO"
+#define OBS_BLANK	"       "
+#define TOP_ROW         9
+#define BOT_ROW         41
+#define LEFT_EDGE       20
+#define RIGHT_EDGE      85
+
+#define X_INIT          52
+#define Y_INIT          40
+#define TIME		20000
+#define NUM_ROAD	(BOT_ROW-TOP_ROW-4)/2
+#define LENGTH		6
+
+struct ppball{
+                int y_pos, x_pos;
+                int y_dir, x_dir;
+                char symbol;
+};
+
+struct obstacle{
+		char *str;
+		int idx;
+		int row, col;
+		int delay;
+		int dir;
+};
+
